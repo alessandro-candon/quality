9f3a144f1db2b3fddc51fe984c8f020da8627d77
Alessandro Candon <alessandro.candon@decathlon.com>
2019-01-18 16:19:53 +0100

    phpcs fail

---
+++ README.md
@@ -0,0 +1,5 @@
+# COMMAND LIST 
+
+### CODE STYLE
+
+`phpcs --standard=PSR2 --extensions=php --encoding=utf8 --report=full src`
---
+++ src/HeartRateCalculator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace src;
+
+/**
+ * Class HeartRateCalculator
+ * @package src
+ */
+class HeartRateCalculator
+{
+    /**
+     * @param bool $isMale
+     * @param int $age
+     * @return float|int
+     * @throws \Exception
+     */
+    public function getMax(bool $isMale, int $age){
+        if($age > 13 ) {
+            if (true === $isMale){return 214 - 0.8 * $age;} else { return 209 - 0.7 * $age;}
+        }
+        throw new \Exception('Age not valid');
+    }
+}
