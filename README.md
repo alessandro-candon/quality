6614de3b796fec3eaebd025f3185952c7bd22060
Alessandro Candon <alessandro.candon@decathlon.com>
2019-01-18 16:29:35 +0100

    php cs success

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ### CODE STYLE
 
-`phpcs --standard=PSR2 --extensions=php --encoding=utf8 --report=full src`
+- `phpcs --standard=PSR2 --extensions=php --encoding=utf8 --report=full src`
+
+- `phpcbf src`
--- src/HeartRateCalculator.php
+++ src/HeartRateCalculator.php
@@ -14,9 +14,14 @@ class HeartRateCalculator
      * @return float|int
      * @throws \Exception
      */
-    public function getMax(bool $isMale, int $age){
-        if($age > 13 ) {
-            if (true === $isMale){return 214 - 0.8 * $age;} else { return 209 - 0.7 * $age;}
+    public function getMax(bool $isMale, int $age)
+    {
+        if ($age > 13) {
+            if (true === $isMale) {
+                return 214 - 0.8 * $age;
+            } else {
+                return 209 - 0.7 * $age;
+            }
         }
         throw new \Exception('Age not valid');
     }
